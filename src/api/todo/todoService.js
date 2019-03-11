22e241d61a75cc65b209d61bffd8caaa437ea53d
Francelio Alencar <francelio.si@gmail.com>
2019-03-11 19:29:33 -0300

    add mapping of routes

--- src/api/todo/todoService.js
+++ src/api/todo/todoService.js
@@ -1,5 +1,4 @@
 const Todo = require('./todo')
-Todo.methods(['get','post','put','delete',])
-Todo.updateOptions({ new:true, runValidators:true})
-
-module.exports = Todos
+Todo.methods(['get', 'post', 'put', 'delete']) 
+Todo.updateOptions({new: true, runValidators: true})
+module.exports = Todo
--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,8 @@
 const port = 3003
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') 
 const express = require('express')
-const server = express()
-
-server.use(bodyParser.urlencoded({extended:true}))
-server.use(bodyParser.json)
-
-server.listen(port,function (){
-    console.log(`BACKEND is runing on port ${port}.`)
-})
+ const server = express()
+server.use(bodyParser.urlencoded({ extended: true })) 
+server.use(bodyParser.json())
+server.listen(port, function() {     console.log(`BACKEND is running on port ${port}.`) })
+module.exports = server
