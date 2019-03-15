21af8178855e0cb54c34e86319436f5fce128c77
Francelio Alencar <francelio.si@gmail.com>
2019-03-14 23:14:02 -0300

    add function key shortcut

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton'
 
-export default props => (
+export default props => {
+    const keyHandler = (e) => {
+      if (e.key === 'Enter'){
+          e.shiftKey ? props.handleSearch() : props.handleAdd()
+      } else if (e.key === 'Escape'){
+        props.handleClear()
+      }
+    }
 
-    <div role='form' className='todoForm'>
+
+    return(
+      <div role='form' className='todoForm'>
       <Grid cols='12 9 10'>
             <input id='description' className='form-control'
             placeholder='Adicione uma tarefa'
+            onKeyUp={keyHandler}
             value = {props.description} 
             onChange={props.handleChange}></input>
       </Grid>
       <Grid cols='12 3 2'>
         <IconButton onClick={props.handleAdd} style='primary' icon='plus' ></IconButton>
         <IconButton onClick={props.handleSearch} style='info' icon='search' ></IconButton>
+        <IconButton onClick={props.handleClear} style='info' icon='close ' ></IconButton>
       </Grid>
     </div>
-  )
+    )
+}
